c0b1f7af5a48a619f16f023c049fdeaa61d534f1
unknown <shaherena@gmail.com>
2024-06-23 23:14:08 +0530

    mcd

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import NavBar from './Components/NavBar'
+import Home from './Components/Home'
+import Menu from './Components/menu'
+import Burgermenu from './Components/burgermenu'
+function App() {
+  return (
+    <div>
+     {/* <NavBar/>
+      <Home/> 
+      <Menu/> */}
+      <Burgermenu/>
+    </div>
+  )
+}
+
+export default App
